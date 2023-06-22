c99caf79988c4f38d6c4303a073e72d9142d69fd
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-06-22 17:45:07 +0900

    コールバックでuuidを作成
    after_createの場合、バリデーションが先に行われてしまいNOT NULL制約に違反するため、before_validationを使用しています。

--- app/models/house_viewing.rb
+++ app/models/house_viewing.rb
@@ -1,3 +1,9 @@
 class HouseViewing < ApplicationRecord
   validates :uuid, presence: true, uniqueness: true
+  before_validation :create_uuid
+
+  def create_uuid
+    uuid = SecureRandom.uuid
+    self.uuid = uuid
+  end
 end
