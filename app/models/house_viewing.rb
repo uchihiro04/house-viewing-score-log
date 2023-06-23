6b6afbfe3b8438e652cd9ca1903e09f4ae647ed1
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-06-23 09:57:29 +0900

    HouseViewingレコード作成時に、Roomレコードを10件作成する形に設定
    HouseViewingレコード作成時に、作成されたレコードに紐づくRoomレコードを10件作成するように設定しました。

--- app/models/house_viewing.rb
+++ app/models/house_viewing.rb
@@ -4,11 +4,19 @@ class HouseViewing < ApplicationRecord
   has_many :rooms
   validates :uuid, presence: true, uniqueness: true
   before_create :set_uuid
+  after_create :create_rooms
 
   def set_uuid
     self.uuid = loop do
       generated_uuid = SecureRandom.uuid
       break generated_uuid unless self.class.exists?(uuid: generated_uuid)
+    end
+  end
+
+  def create_rooms
+    house_viewing_id = self.id
+    10.times do |n|
+      Room.create(name: "#{n + 1}件目", house_viewing_id: house_viewing_id)
     end
   end
 end
