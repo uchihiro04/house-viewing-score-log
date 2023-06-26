2d1eea02b0eed058eb575ab7510266ecf41e7567
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-06-26 14:36:39 +0900

    #78 RoomモデルとScoreモデルを関連付け
    Roomを1、Scoreを多とする関連付けを行いました。

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Room < ApplicationRecord
+  has_many :scores, dependent: :destroy
   belongs_to :house_viewing
-  validates :name, presence: true
 end
--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Score < ApplicationRecord
+  belongs_to :room
 end
