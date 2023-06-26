c1128820e2c5a24b3d1da0382fb5d7b21d9904d8
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-06-26 09:47:31 +0900

    roomsコントローラのindexアクションにコードを追加
    roomsコントローラのindexアクションに、HouseViewingデータに紐づいたRoomデータを10件表示できるよう、インスタンス変数roomsを定義しました。

--- app/controllers/house_viewings/rooms_controller.rb
+++ app/controllers/house_viewings/rooms_controller.rb
@@ -1,3 +1,6 @@
 class HouseViewings::RoomsController < ApplicationController
-  def index; end
+  def index
+    house_viewing = HouseViewing.find_by!(uuid: params[:house_viewing_uuid])
+    @rooms = house_viewing.rooms
+  end
 end
