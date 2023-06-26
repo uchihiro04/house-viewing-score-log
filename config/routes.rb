c70cc2392439bc2ada53bd1d3d78cfff2865d92d
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-06-26 17:22:32 +0900

    #107 ルーティングを設定
    house_viewings/rooms/scores#newコントローラに関するPathを設定しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :house_viewings, param: :uuid do 
-    resources :rooms, only: [:index], module: :house_viewings
+    resources :rooms, only: [:index], module: :house_viewings do
+      resources :scores, only: [:new], module: :rooms
+    end
   end
 end
