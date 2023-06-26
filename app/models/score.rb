7db65f99ff90e64c045f97d0aefaca0da3f7cc73
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-06-26 14:42:50 +0900

    #78 バリデーションを追加
    NOT NULL、数値の範囲に関するバリデーションをそれぞれ追加しました。

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,4 +2,13 @@
 
 class Score < ApplicationRecord
   belongs_to :room
+  validates :reviewer_name, presence: true
+  validates :living_room, presence: true, numericality: { in: 1..5 }
+  validates :storage, presence: true, numericality: { in: 1..5 }
+  validates :kitchen, presence: true, numericality: { in: 1..5 }
+  validates :bath, presence: true, numericality: { in: 1..5 }
+  validates :toilet, presence: true, numericality: { in: 1..5 }
+  validates :equipment, presence: true, numericality: { in: 1..5 }
+  validates :surroundings, presence: true, numericality: { in: 1..5 }
+  validates :rent, presence: true, numericality: { in: 1..5 }
 end
