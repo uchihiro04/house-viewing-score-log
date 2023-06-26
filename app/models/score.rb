6c7bdeb4a8c7668e5e6c295b5898bdb968ecbdfd
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-06-26 16:43:26 +0900

    バリデーションにwith_optionsを使用
    バリデーションにwith_optionsを使用することで、バリデーションに関するコードをDRYにしました。

--- app/models/score.rb
+++ app/models/score.rb
@@ -2,13 +2,17 @@
 
 class Score < ApplicationRecord
   belongs_to :room
-  validates :reviewer_name, presence: true
-  validates :living_room, presence: true, numericality: { in: 1..5 }
-  validates :storage, presence: true, numericality: { in: 1..5 }
-  validates :kitchen, presence: true, numericality: { in: 1..5 }
-  validates :bath, presence: true, numericality: { in: 1..5 }
-  validates :toilet, presence: true, numericality: { in: 1..5 }
-  validates :equipment, presence: true, numericality: { in: 1..5 }
-  validates :surroundings, presence: true, numericality: { in: 1..5 }
-  validates :rent, presence: true, numericality: { in: 1..5 }
+  with_options presence: true do
+    validates :reviewer_name
+    with_options numericality: { in: 1..5 } do
+      validates :living_room
+      validates :storage
+      validates :kitchen
+      validates :bath
+      validates :toilet
+      validates :equipment
+      validates :surroundings
+      validates :rent
+    end
+  end
 end
