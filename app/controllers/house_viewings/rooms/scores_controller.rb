6d7878bb88d06f9531c6e80675e9b991a0465bc1
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-06-28 11:38:39 +0900

    #107 存在しないroom_idでアクセスできる不具合を修正
    スコア入力ページについて、存在しないroom_idでもアクセスできてしまう不具合を修正しました。

--- app/controllers/house_viewings/rooms/scores_controller.rb
+++ app/controllers/house_viewings/rooms/scores_controller.rb
@@ -4,7 +4,8 @@ module HouseViewings
   module Rooms
     class ScoresController < ApplicationController
       def new
-        @room = Room.find(params[:room_id])
+        house_viewing = HouseViewing.find_by!(uuid: params[:house_viewing_uuid])
+        @room = house_viewing.rooms.find(params[:room_id])
       end
     end
   end
