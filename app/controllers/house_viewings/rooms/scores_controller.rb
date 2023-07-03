bdfd3ee930f08fce7ace2b291de35e19ce851fb7
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-03 14:25:59 +0900

    #82 before_actionのメソッドを追加
    部屋名の取得についてメソッド化し、before_actionに追加しました。

--- app/controllers/house_viewings/rooms/scores_controller.rb
+++ app/controllers/house_viewings/rooms/scores_controller.rb
@@ -5,6 +5,7 @@ module HouseViewings
     class ScoresController < ApplicationController
       before_action :set_house_viewing, :set_room, only: %i[new create edit update]
       before_action :set_score, only: %i[edit update]
+      before_action :fetch_room_name, only: %i[create update]
 
       def new
         @score = Score.new
@@ -13,9 +14,8 @@ module HouseViewings
       def edit; end
 
       def create
-        room_name = params[:score][:room][:name]
         @score = @room.scores.new(score_params)
-        @room.name = room_name if @room.name != room_name
+        @room.name = @room_name if @room.name != @room_name
 
         if @room.save
           redirect_to house_viewing_rooms_path, notice: t('notice.create', model: @score.class.model_name.human)
@@ -25,8 +25,7 @@ module HouseViewings
       end
 
       def update
-        room_name = params[:score][:room][:name]
-        @room.name = room_name if @room.name != room_name
+        @room.name = @room_name if @room.name != @room_name
 
         if @score.update(score_params)
           redirect_to house_viewing_rooms_path, notice: t('notice.update', model: @score.class.model_name.human)
@@ -43,6 +42,10 @@ module HouseViewings
 
       def set_room
         @room = @house_viewing.rooms.find(params[:room_id])
+      end
+
+      def fetch_room_name
+        @room_name = params[:score][:room][:name]
       end
 
       def set_score
