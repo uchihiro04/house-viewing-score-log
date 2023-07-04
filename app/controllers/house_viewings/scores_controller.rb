92de2a165e91915bd5ba56c6c6a368e90219b37f
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-04 15:25:04 +0900

    #49 定数名を変更
    定数名NUMBER_OF_DISPLAYについて、表示が上位2件であることを具体的にイメージできるように名前をTOP_TWOに変更しました。

--- app/controllers/house_viewings/scores_controller.rb
+++ app/controllers/house_viewings/scores_controller.rb
@@ -2,13 +2,13 @@
 
 module HouseViewings
   class ScoresController < ApplicationController
-    NUMBER_OF_DISPLAY = 2
+    TOP_TWO = 2
 
     def index
       house_viewing = HouseViewing.find_by!(uuid: params[:house_viewing_uuid])
       @rooms = house_viewing.rooms.order(:created_at)
 
-      @top_two_room_scores = @rooms.max_by(NUMBER_OF_DISPLAY, &:average_total_score)
+      @top_two_room_scores = @rooms.max_by(TOP_TWO, &:average_total_score)
     end
   end
 end
