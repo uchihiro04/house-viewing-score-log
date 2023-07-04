659b0ed22cef619904085636a4a2bbd33e0459f0
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-04 16:40:06 +0900

    #17 ルーティングを設定
    スコア詳細ページに関するルーティングを設定しました。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :house_viewings, param: :uuid do 
     resources :scores, only: [:index], module: :house_viewings
     resources :rooms, only: [:index], module: :house_viewings do
-      resources :scores, only: [:new, :create, :edit, :update], module: :rooms
+      resources :scores, only: [:index, :new, :create, :edit, :update], module: :rooms
     end
   end
 end
