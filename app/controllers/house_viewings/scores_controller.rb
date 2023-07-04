ff5159588bc7bb335b7bbd746315b9087a4b2b16
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-04 13:05:58 +0900

    #49 変数を定数に変更
    表示件数に関する変数について、基本的に数値は変更されないため定数に変更しました。

--- app/controllers/house_viewings/scores_controller.rb
+++ app/controllers/house_viewings/scores_controller.rb
@@ -2,12 +2,13 @@
 
 module HouseViewings
   class ScoresController < ApplicationController
+    NUMBER_OF_DISPLAY = 2
+
     def index
       house_viewing = HouseViewing.find_by!(uuid: params[:house_viewing_uuid])
       @rooms = house_viewing.rooms.order(:created_at)
 
-      number_of_display = 2
-      @top_two_room_scores = @rooms.max_by(number_of_display, &:average_total_score)
+      @top_two_room_scores = @rooms.max_by(NUMBER_OF_DISPLAY, &:average_total_score)
     end
   end
 end
