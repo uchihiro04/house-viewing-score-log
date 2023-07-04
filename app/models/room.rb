07efbaf000e1bd1064ced22ba2cd415e376ec78d
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-04 15:50:26 +0900

    #49 早期returnを使用する形に変更
    Roomモデルのaverage_total_scoreメソッド、average_scoreメソッドについて、早期returnを使う形に変更しました。

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,19 +8,13 @@ class Room < ApplicationRecord
 
   def average_total_score
     sum_total_scores = Score::EVALUATION_ITEMS.sum { |attribute_name| scores.sum(attribute_name) }.to_f
-    if number_of_scores.positive?
-      (sum_total_scores / number_of_scores).round
-    else
-      0
-    end
+    return 0 unless scores.present?
+    (sum_total_scores / number_of_scores).round
   end
 
   def average_score(attribute_name)
-    if number_of_scores.positive?
-      (scores.sum(attribute_name).to_f / number_of_scores).round
-    else
-      0
-    end
+    return 0 unless scores.present?
+    (scores.sum(attribute_name).to_f / number_of_scores).round
   end
 
   private
