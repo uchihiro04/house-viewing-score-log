bde8b0f2edc3d429b8db71db87f22cd64a3a7c35
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-04 15:55:56 +0900

    #49 number_of_scoresメソッドの削除
    コードの修正により使用する箇所が少なくなったため、メソッドとして定義せず中身のコードをそのまま使用する形に変更しました。

--- app/models/room.rb
+++ app/models/room.rb
@@ -9,17 +9,11 @@ class Room < ApplicationRecord
   def average_total_score
     sum_total_scores = Score::EVALUATION_ITEMS.sum { |attribute_name| scores.sum(attribute_name) }.to_f
     return 0 unless scores.present?
-    (sum_total_scores / number_of_scores).round
+    (sum_total_scores / scores.length).round
   end
 
   def average_score(attribute_name)
     return 0 unless scores.present?
-    (scores.sum(attribute_name).to_f / number_of_scores).round
-  end
-
-  private
-
-  def number_of_scores
-    scores.length
+    (scores.sum(attribute_name).to_f / scores.length).round
   end
 end
