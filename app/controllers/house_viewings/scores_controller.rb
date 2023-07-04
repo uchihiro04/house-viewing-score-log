5e2f3e638cf2926b53e8a98dacf004b4afd30998
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-04 09:21:40 +0900

    #49 スコア上位の2部屋を表示
    スコアが上位の2部屋の部屋名・スコアをそれぞれ表示しました。

--- app/controllers/house_viewings/scores_controller.rb
+++ app/controllers/house_viewings/scores_controller.rb
@@ -5,6 +5,12 @@ module HouseViewings
     def index
       house_viewing = HouseViewing.find_by!(uuid: params[:house_viewing_uuid])
       @rooms = house_viewing.rooms.order(:created_at)
+
+      room_scores = {}
+      @rooms.each do |room|
+        room_scores[room.name] = room.average_total_score
+      end
+      @top_two_scores = room_scores.sort_by { |_key, value| -value }.first(2)
     end
   end
 end
