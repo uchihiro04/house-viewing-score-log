43ff0e0e60ed085033a72a366546ac409676c9b1
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-05 16:24:32 +0900

    #29 newメソッドをcreateメソッドに変更
    HouseViewingレコードの作成について、バリデーションなどによる失敗が原則発生しないため、saveメソッドをcreateメソッドに変更しました。また、saveメソッドに関する条件分岐を削除しました。

--- app/controllers/house_viewings_controller.rb
+++ app/controllers/house_viewings_controller.rb
@@ -2,13 +2,8 @@
 
 class HouseViewingsController < ApplicationController
   def create
-    house_viewing = HouseViewing.new
-
-    if house_viewing.save
-      redirect_to house_viewing_rooms_path(house_viewing),
-                  notice: t('notice.create', model: house_viewing.model_name.human)
-    else
-      render 'home/index', status: :unprocessable_entity
-    end
+    house_viewing = HouseViewing.create
+    redirect_to house_viewing_rooms_path(house_viewing),
+                notice: t('notice.create', model: house_viewing.model_name.human)
   end
 end
