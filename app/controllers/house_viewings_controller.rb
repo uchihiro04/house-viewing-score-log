59d834b374e256659581813fda8743a094de7822
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-05 15:34:51 +0900

    #29 「はじめる」ボタンの作成
    TOPページに「はじめる」ボタンを作成しました。押すとHouseViewingレコードを作成・保存し、保存したHouseViewingレコードに応じた内見一覧ページに遷移します。

---
+++ app/controllers/house_viewings_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class HouseViewingsController < ApplicationController
+  def create
+    @house_viewing = HouseViewing.new
+
+    if @house_viewing.save
+      redirect_to house_viewing_rooms_path(@house_viewing),
+                  notice: t('notice.create', model: @house_viewing.model_name.human)
+    else
+      render 'home/index', status: :unprocessable_entity
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
 
-  resources :house_viewings, param: :uuid do 
+  resources :house_viewings, only: [:create], param: :uuid do 
     resources :scores, only: [:index], module: :house_viewings
     resources :rooms, only: [:index], module: :house_viewings do
       resources :scores, only: [:index, :new, :create, :edit, :update], module: :rooms
