0eb73f644d0dec731a32598ff0c4d397f9f82c1f
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-05 09:13:30 +0900

    #49 rubocopの指摘を修正
    present?メソッドやガード節についてrubocopから指摘を受けたため、修正しました。

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,12 +8,14 @@ class Room < ApplicationRecord
 
   def average_total_score
     sum_total_scores = Score::EVALUATION_ITEMS.sum { |attribute_name| scores.sum(attribute_name) }.to_f
-    return 0 unless scores.present?
+    return 0 if scores.blank?
+
     (sum_total_scores / scores.length).round
   end
 
   def average_score(attribute_name)
-    return 0 unless scores.present?
+    return 0 if scores.blank?
+
     (scores.sum(attribute_name).to_f / scores.length).round
   end
 end
