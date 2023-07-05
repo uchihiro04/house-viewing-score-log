4e18bd85d2e7d4930d07d9e745c7da6fb06257af
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-05 16:19:56 +0900

    #29 インスタンス変数をローカル変数に変更
    HouseViewingsコントローラのcreateアクションについて、スコープを狭めるためにインスタンス変数からローカル変数に変更しました。

--- app/controllers/house_viewings_controller.rb
+++ app/controllers/house_viewings_controller.rb
@@ -2,11 +2,11 @@
 
 class HouseViewingsController < ApplicationController
   def create
-    @house_viewing = HouseViewing.new
+    house_viewing = HouseViewing.new
 
-    if @house_viewing.save
-      redirect_to house_viewing_rooms_path(@house_viewing),
-                  notice: t('notice.create', model: @house_viewing.model_name.human)
+    if house_viewing.save
+      redirect_to house_viewing_rooms_path(house_viewing),
+                  notice: t('notice.create', model: house_viewing.model_name.human)
     else
       render 'home/index', status: :unprocessable_entity
     end
