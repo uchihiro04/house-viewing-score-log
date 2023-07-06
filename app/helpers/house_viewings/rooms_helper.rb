6b03e5c5bedf26ea8f3802be62143d2dc87c4ba7
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-06 17:00:59 +0900

    #90 valueの中身をルーティングヘルパーを使用する形に変更
    inputタグのvalueについて、ルーティングヘルパーを使用した形に変更しました。

--- app/helpers/house_viewings/rooms_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module HouseViewings
-  module RoomsHelper
-    def description_page_url(house_viewing)
-      "#{request.base_url}/description/house_viewings/#{house_viewing.uuid}"
-    end
-  end
-end
