c1ec4d55a743a6f73a567c7de2c7ed6167ea7350
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-06 17:10:16 +0900

    #90 メソッド名を変更
    clipboard_controller.jsのメソッドcopyDescriptionPageUrlについて、メソッド名をcopySharedUrlに変更しました。

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "url" ]
   
-  copyDescriptionPageUrl() {
+  copySharedUrl() {
     navigator.clipboard.writeText(this.urlTarget.value)
   }
 }
