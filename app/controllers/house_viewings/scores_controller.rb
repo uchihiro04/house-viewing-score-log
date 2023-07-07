1211d84d8369e4131aeb2748d35e679baf969853
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-07 10:48:45 +0900

    #80 フラッシュメッセージを設定
    スコア未入力により内見一覧ページにリダイレクトした際、フラッシュメッセージを表示するよう設定しました。

--- app/controllers/house_viewings/scores_controller.rb
+++ app/controllers/house_viewings/scores_controller.rb
@@ -8,9 +8,15 @@ module HouseViewings
       house_viewing = HouseViewing.find_by!(uuid: params[:house_viewing_uuid])
       @rooms = house_viewing.rooms.includes(:scores).order(:created_at)
 
-      redirect_to house_viewing_rooms_path if @rooms.all? { |room| room.scores.blank? }
+      redirect_to house_viewing_rooms_path, alert: t('alert.no_scores') if rooms_score_blank?
 
       @top_two_rooms = @rooms.max_by(TOP_TWO, &:average_total_score)
+    end
+
+    private
+
+    def rooms_score_blank?
+      @rooms.all? { |room| room.scores.blank? }
     end
   end
 end
