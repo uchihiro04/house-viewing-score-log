cec88dddfc6dbe572a72993949cfa5143344543d
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-07 09:27:57 +0900

    #80 スコア未入力時にスコア結果ページに遷移した場合、内見一覧ページにリダイレクトする形に変更

--- app/controllers/house_viewings/scores_controller.rb
+++ app/controllers/house_viewings/scores_controller.rb
@@ -8,6 +8,8 @@ module HouseViewings
       house_viewing = HouseViewing.find_by!(uuid: params[:house_viewing_uuid])
       @rooms = house_viewing.rooms.includes(:scores).order(:created_at)
 
+      redirect_to house_viewing_rooms_path if @rooms.all? { |room| room.scores.blank? }
+
       @top_two_rooms = @rooms.max_by(TOP_TWO, &:average_total_score)
     end
   end
