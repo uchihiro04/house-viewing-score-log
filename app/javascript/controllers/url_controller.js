ed640a1a2c4701b57de0b709234047ff85644768
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-13 10:36:02 +0900

    #102 お部屋一覧をテーブルからリストに変更
    お部屋一覧について、テーブルからリストに変更しました。また、不要になったJavaScriptファイルを削除しました。

--- app/javascript/controllers/url_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  transitionToUrl(event) {
-    const url = event.currentTarget.dataset['href']
-    window.location.href = url
-  }
-}
