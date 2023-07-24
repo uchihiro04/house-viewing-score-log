b1bf0c2b2dbee811bb6daf550d8caac0c5bd546e
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-24 14:51:42 +0900

    #144 contextを削除
    メソッドが条件によって返り値を変えない場合、contextは不要ではないかとレビューをいただいたため削除しました。

--- spec/models/house_viewing_spec.rb
+++ spec/models/house_viewing_spec.rb
@@ -4,11 +4,9 @@ require 'rails_helper'
 
 RSpec.describe HouseViewing, type: :model do
   describe '#to_param' do
-    context 'HouseViewingのレコードをもとにURLが作成されたとき' do
-      it 'pathとしてuuidが返されること' do
-        house_viewing = build(:house_viewing)
-        expect(house_viewing.to_param).to eq(house_viewing.uuid)
-      end
+    it 'pathとしてuuidが返されること' do
+      house_viewing = build(:house_viewing)
+      expect(house_viewing.to_param).to eq(house_viewing.uuid)
     end
   end
 end
