830f83a31029e54fc6c7fde8ed97ecd0a8d38d76
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-25 13:07:20 +0900

    #145 Scoreのテストデータについて、評価項目の得点を修正
    Scoreのテストデータの評価項目について、sequenceを使う方法から、randメソッドを使う方法に修正しました。

--- spec/factories/score.rb
+++ spec/factories/score.rb
@@ -2,14 +2,14 @@
 
 FactoryBot.define do
   factory :score do
-    sequence(:reviewer_name) { |n| "ユーザー#{n}" }
-    sequence(:living_room) { |n| n }
-    sequence(:storage) { |n| n }
-    sequence(:kitchen) { |n| n }
-    sequence(:bath) { |n| n }
-    sequence(:toilet) { |n| n }
-    sequence(:equipment) { |n| n }
-    sequence(:surroundings) { |n| n }
-    sequence(:rent) { |n| n }
+    reviewer_name { rand(1..5) }
+    living_room { rand(1..5) }
+    storage { rand(1..5) }
+    kitchen { rand(1..5) }
+    bath { rand(1..5) }
+    toilet { rand(1..5) }
+    equipment { rand(1..5) }
+    surroundings { rand(1..5) }
+    rent { rand(1..5) }
   end
 end
