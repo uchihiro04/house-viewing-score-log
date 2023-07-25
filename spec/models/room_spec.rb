abac955e15b4fd1a95709aee2865c30bf42c5aae
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-25 11:55:04 +0900

    #145 スコアの平均値を返すテストについて、reloadを使用する形に変更
    #average_scoreメソッドのスコアの平均値を返すテストについて、reloadを用いてroomの情報を更新する形に変更しました。

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -6,13 +6,12 @@ RSpec.describe Room, type: :model do
   describe '#average_score' do
     let(:house_viewing) { create(:house_viewing) }
     let(:room) { create(:room, house_viewing:) }
-    let(:scores) { create_list('score', 3, room:) }
+    let!(:scores) { create_list('score', 3, room:) }
 
     context 'スコアが存在する場合' do
       it 'スコアの平均値を返すこと' do
-        room.scores = scores
         # 1点、2点、3点の平均値を求めているため、平均値は2.0点になる。
-        expect(room.average_score(Score::EVALUATION_ITEMS.first)).to eq 2.0
+        expect(room.reload.average_score(Score::EVALUATION_ITEMS.first)).to eq 2.0
       end
     end
 
