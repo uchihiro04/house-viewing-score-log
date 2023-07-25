b931e0a16edd907dfc2c484c2ccdf6f4a0f40588
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-25 14:48:30 +0900

    #145 average_total_scoreメソッドについて、スコアが存在する場合のテストを作成

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -3,16 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  describe '#average_score' do
-    let(:house_viewing) { create(:house_viewing) }
-    let(:room) { create(:room, house_viewing:) }
+  let(:house_viewing) { create(:house_viewing) }
+  let(:room) { create(:room, house_viewing:) }
 
-    before do
-      test_values.each do |test_value|
-        create(:score, room:, Score::EVALUATION_ITEMS.first => test_value)
+  before do
+    test_values.each do |test_value|
+      evaluation_items = { room: }
+      Score::EVALUATION_ITEMS.each do |item|
+        evaluation_items[item] = test_value
       end
+      create(:score, evaluation_items)
     end
+  end
 
+  describe '#average_score' do
     context 'スコアが存在する場合' do
       let(:test_values) { [1, 2, 3] }
 
@@ -27,6 +31,17 @@ RSpec.describe Room, type: :model do
 
       it '0を返すこと' do
         expect(room.reload.average_score(Score::EVALUATION_ITEMS.first)).to eq 0
+      end
+    end
+  end
+
+  describe '#average_total_score' do
+    context 'スコアが存在する場合' do
+      let(:test_values) { [1, 2, 3] }
+
+      it '合計スコアの平均値を返すこと' do
+        # 合計点が8点、16点、24点の平均値を求めているため、平均値は16点になる。
+        expect(room.reload.average_total_score).to eq 16
       end
     end
   end
