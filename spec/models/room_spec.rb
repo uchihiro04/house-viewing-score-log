299c8cbe83488b71415f370d8406e7e35b00ced7
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-25 13:29:40 +0900

    #145 Scoreデータの作成方法を変更
    Scoreデータについて、create_listを用いた方法から、beforeとcreateを用いた方法に変更しました。

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -6,9 +6,16 @@ RSpec.describe Room, type: :model do
   describe '#average_score' do
     let(:house_viewing) { create(:house_viewing) }
     let(:room) { create(:room, house_viewing:) }
-    let!(:scores) { create_list('score', 3, room:) }
+
+    before do
+      test_values.each do |test_value|
+        create(:score, room:, living_room: test_value)
+      end
+    end
 
     context 'スコアが存在する場合' do
+      let(:test_values) { [1, 2, 3] }
+
       it 'スコアの平均値を返すこと' do
         # 1点、2点、3点の平均値を求めているため、平均値は2.0点になる。
         expect(room.reload.average_score(Score::EVALUATION_ITEMS.first)).to eq 2.0
@@ -16,7 +23,7 @@ RSpec.describe Room, type: :model do
     end
 
     context 'スコアが存在しない場合' do
-      let(:scores) { nil }
+      let(:test_values) { [] }
 
       it '0を返すこと' do
         expect(room.reload.average_score(Score::EVALUATION_ITEMS.first)).to eq 0
