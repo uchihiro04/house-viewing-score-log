a917dec0d551fed32a37c794454bb313d16f44d8
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-28 15:13:35 +0900

    #153 N+1問題が発生している箇所を修正

--- app/controllers/dashboard/rooms_controller.rb
+++ app/controllers/dashboard/rooms_controller.rb
@@ -5,7 +5,7 @@ module Dashboard
     def index
       min_score = params[:min_score].to_i
       max_score = params[:max_score].present? ? params[:max_score].to_i : 40
-      @rooms = Room.all.select do |room|
+      @rooms = Room.all.includes(:scores).select do |room|
         min_score <= room.average_total_score && room.average_total_score <= max_score
       end
     end
