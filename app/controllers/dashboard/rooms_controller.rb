ed8d03a98100d3634b3f4a5b35b750a223458de9
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-28 13:37:18 +0900

    #153 フォームが空白時の数値を設定
    From側の検索フォームが空白の時は0、To側の検索フォームが空白の時は40が入る形に変更しました。

--- app/controllers/dashboard/rooms_controller.rb
+++ app/controllers/dashboard/rooms_controller.rb
@@ -4,7 +4,7 @@ module Dashboard
   class RoomsController < ApplicationController
     def index
       min_score = params[:min_score].to_i
-      max_score = params[:max_score].to_i
+      max_score = params[:max_score].present? ? params[:max_score].to_i : 40
       @rooms = Room.all.select do |room|
         min_score <= room.average_total_score && room.average_total_score <= max_score
       end
