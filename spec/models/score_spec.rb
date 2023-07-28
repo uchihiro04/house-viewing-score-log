f6b30c4fd39742d20306a50e67752f5c1bcc45f3
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-28 18:02:30 +0900

    #149 評価項目の数値が6以上の場合のテストを作成

--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -15,13 +15,22 @@ RSpec.describe Score, type: :model do
         expect(score.errors.full_messages).to include('リビングは1..5の範囲に含めてください')
       end
     end
-  end
 
-  context '数値が1~5の場合' do
-    let(:score) { build(:score, room:, living_room: 3) }
+    context '数値が1~5の場合' do
+      let(:score) { build(:score, room:, living_room: 3) }
 
-    it 'リビングの数値が有効であること' do
-      expect(score).to be_valid
+      it 'リビングの数値が有効であること' do
+        expect(score).to be_valid
+      end
+    end
+
+    context '数値が6以上の場合' do
+      let(:score) { build(:score, room:, living_room: 6) }
+
+      it 'リビングの数値が無効であること' do
+        score.valid?
+        expect(score.errors.full_messages).to include('リビングは1..5の範囲に含めてください')
+      end
     end
   end
 end
