fbab6a80ccb4301ac35478f94ec465bf09dc1e84
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-28 16:54:35 +0900

    #153 average_scoreメソッドのクエリの発行を減少
    average_scoreメソッドについて、arrayクラスのsumを使うことでクエリの発行を減らしました。

--- app/models/room.rb
+++ app/models/room.rb
@@ -17,7 +17,7 @@ class Room < ApplicationRecord
   def average_score(attribute_name)
     return 0 if scores.blank?
 
-    (scores.sum(attribute_name).to_f / scores.length).round(1)
+    (scores.sum { |score| score.send(attribute_name) }.to_f / scores.length).round(1)
   end
 
   def reviewer_names
