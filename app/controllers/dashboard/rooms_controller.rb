5723a4b38e8e2912d0af2a5bdb6172f987db864e
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-31 14:40:30 +0900

    #153 Dashboard::Roomsコントローラのindexアクションを修正
    変数min_scoreについて、三項演算子を使用する形に変更しました。

--- app/controllers/dashboard/rooms_controller.rb
+++ app/controllers/dashboard/rooms_controller.rb
@@ -3,7 +3,7 @@
 module Dashboard
   class RoomsController < ApplicationController
     def index
-      min_score = params[:min_score].to_i
+      min_score = params[:min_score].present? ? params[:min_score].to_i : 0
       max_score = params[:max_score].present? ? params[:max_score].to_i : 40
       @rooms = Room.all.includes(:scores).select do |room|
         min_score <= room.average_total_score && room.average_total_score <= max_score
