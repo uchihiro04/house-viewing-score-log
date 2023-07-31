e0e9f3efee809e652c58cc3e9da130c797a5d7d4
uchida_luxiar <hiroto.uchida@luxiar.com>
2023-07-31 14:58:22 +0900

    #153 rubocopの指摘を修正
    Dashboard::Roomsコントローラについて、rubocopからの指摘を受けてメソッドを分割しました。

--- app/controllers/dashboard/rooms_controller.rb
+++ app/controllers/dashboard/rooms_controller.rb
@@ -3,11 +3,19 @@
 module Dashboard
   class RoomsController < ApplicationController
     def index
-      min_score = params[:min_score].present? ? params[:min_score].to_i : 0
-      max_score = params[:max_score].present? ? params[:max_score].to_i : 40
       @rooms = Room.all.includes(:scores).select do |room|
-        min_score <= room.average_total_score && room.average_total_score <= max_score
+        room.average_total_score.between?(min_score, max_score)
       end
+    end
+
+    private
+
+    def min_score
+      params[:min_score].present? ? params[:min_score].to_i : 0
+    end
+
+    def max_score
+      params[:max_score].present? ? params[:max_score].to_i : 40
     end
   end
 end
